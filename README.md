cd27c507bc7302ae72e67196dfd347fa9dec4797
Mandy Tsang <mandy.tsang007@gmail.com>
2022-01-11 23:28:19 -0800

    debug and add README.md

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Weather-Dashboard
+
+The purpose of this Weather Dashboard is to provide travelers a weather outlook of the cities that they have selected. This application is built with HTML, CSS, JavaScript, and also power by jQuery and Bootstrap. Travelers are able to do a global city search. This application utilizes a third-party API, OpenWeather Map, to retrieve weather data for cities. LocalStorage feature is also incorperated in this application so that users are able to see their search history on the browser. 
+
+
+## Getting Started
+
+When it runs on a browser, a search box will be displayed asking the user to make a city search. Once it is entered, the city name, the temperature, the humidity, the wind speed, and the UV index of the current date will be displayed in addition to the five-day weather forecast for that city. The search history will be stored in a localStorage so that users can easily access to their previous search. 
+
+
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [w3schools](https://www.w3schools.com/js/default.asp)
+* [Moment.js](https://momentjs.com/docs/)
+* [jQuery User Interface](https://jqueryui.com)
+* [Bootstrap](https://getbootstrap.com)
+
+
+## Deployed Link
+
+* [Deployed Link to Live Site](https://mandytsang007.github.io/PasswordGenerator/)
+* [Link to Github](https://github.com/MANDYTSANG007/PasswordGenerator)
+
+## Design Layout
+
+![alt text](passwordGenerator.png)
+
+## Authors
+
+* **Mandy Tsang** 
+
+- [Portfolio](https://mandytsang007.github.io/HMT-Portfolio/)
+- [Github](https://github.com/MANDYTSANG007)
+- [LinkedIn](https://www.linkedin.com/in/mandy-tsang-896b2682)
+
+
+## License
+
+This project is licensed under the MIT License 
+
--- script.js
+++ script.js
@@ -7,9 +7,8 @@ var forecastContainer = document.querySelector("#forecast-container");
 var buttonElm = document.getElementById("search-city")
 var cities = [];
 
-
 function searchCity(city) {
-    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;  //construct a query URL "https://openweathermap.org/current#name" using string concatenation to store the OW current weather data URL and the necessary variables
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;  //construct a query URL "https://openweathermap.org/current#name" using string concatenation to store the OW current weather data URL and the necessary variables
 
     $.ajax({
         url: queryUrl,
@@ -104,7 +103,7 @@ function fiveDayForecast(city) {
     $("#5").empty();
 
     //var queryUrlForecast = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + lat + "&lon=" + lon + "&dt={time}" + "&appid=" + APIKey;  //construct a query for retrieving data again
-    var queryUrlForecast = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+    var queryUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
     $.ajax({
         url: queryUrlForecast,
         method: "GET"
@@ -125,7 +124,7 @@ function fiveDayForecast(city) {
 
             function changeTemp() {                                                  //convert K to °F
                 var fahrenheit = parseFloat(response.list[i].main.temp);
-                var convertTemp = Math((fahrenheit - 273.15) * (9 / 5) + 32);
+                var convertTemp = Math.round((fahrenheit - 273.15) * (9 / 5) + 32);
                 tempForecast.text("Temp: " + convertTemp + "°F");
             }
             changeTemp();
@@ -145,48 +144,3 @@ function fiveDayForecast(city) {
 
 
 
-
-
-//use geocode latitude and longtitude and one call combination to get UI index
-
-//Request for data from OpenWeather API
-/*fetch(queryUrl)                                 //call the Fetch API to pass the query/request URL in as a parameter
-    .then(function(response){
-        return response.json();                 //the original response is from http, we use json()to extract that response
-    }).then(function(data){                       //need to do something to the data
-        console.log(data);
-        var list = data.list;                   //get a list from the data
-
-        for(var item of list){                  //loop through the list, get the following data
-            var main = item.main;
-            var temp = main.temp;
-            var day = item.dt;
-            var wind = item.wind;
-            var humidity = main.humidity;
-        }
-    });
-    */
-
-
-
-
-
-
-
-
-
-/*GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-    -create a user input
-    -create a button for users to submit a search
-        -getElementById("button")
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-*/
